71ec25be88118be82c2908c3b39f0105ccd9e0e8
Guilherme Schmidt <devguilhermeschmidt@gmail.com>
2025-12-10 00:52:57 -0300

    add: estrutura basica java

---
+++ backend/evolvia-api/src/main/java/com/evolvia/evolvia_api/EvolviaApiApplication.java
@@ -0,0 +1,13 @@
+package com.evolvia.evolvia_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvolviaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvolviaApiApplication.class, args);
+	}
+
+}
---
+++ backend/evolvia-api/src/test/java/com/evolvia/evolvia_api/EvolviaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evolvia.evolvia_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvolviaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
